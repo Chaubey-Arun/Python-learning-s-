1ecf35ecdcd62013a8510de1dc0a92648a73b4c4
Arun Kumar Chaubey <chaubeyarun.arun@gmail.com>
2024-01-29 08:47:11 +0530

    'Maximum Number Tested with Negative number  as Previously only Positive number's max is obtained but now sorted'

--- Challenge - Max number.py
+++ Challenge - Max number.py
@@ -1,11 +1,9 @@
 No_Number= int(input("Enter No of Number of number's "))
-max =0 
+max =int(input("Enter Number"))
 count=0
-while No_Number>count:
+while No_Number-1 >count:
     n=int(input("Enter Number"))
     if n>max:
-         max = n
-    else:
-         max = max 
+        max=n
     count+=1
 print("maximum number", max)
