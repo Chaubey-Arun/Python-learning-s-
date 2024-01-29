213bbd87cbc9539f744bf7299e3d3f3c98f70e46
Arun Kumar Chaubey <chaubeyarun.arun@gmail.com>
2024-01-29 08:06:52 +0530

    Challenge -Positive and Negative Numbers

---
+++ Challenge -Positive and Negative Numbers.py
@@ -0,0 +1,15 @@
+No_Number= int(input("Enter No of Number of number's "))
+psum = 0 
+nsum= 0 
+count=0
+while No_Number>count:
+    n=int(input("Enter Number"))
+    if n>0:
+     psum = psum + n
+    elif n<0:
+     nsum = nsum + n
+    count+=1
+print("Sum of Positive ,Negative  Number's ",psum,nsum)
+
+    
+
