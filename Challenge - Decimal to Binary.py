187358914bcfd49fa612ae05ed1ec14156162404
Arun Kumar Chaubey <chaubeyarun.arun@gmail.com>
2024-01-30 03:57:39 +0530

    This Decimal to binary is rectified as Previously the program work for even decimal number as this will work for every number .

--- Challenge - Decimal to Binary.py
+++ Challenge - Decimal to Binary.py
@@ -1,17 +1,10 @@
 n= int(input("Enter No of Number of number's "))
-binary =0 
-sum = 0 
+sum = " "
 while n>0:
     r = n%2 
     n=n//2
-    sum= sum*10 + r 
+    sum= str(r)+sum 
+print(sum)
 
-while sum>0:
-     d = sum%10
-     sum=sum //10
-    
-     binary= binary*10 + d
-
-print("Binary Number is ",binary)
 
 
