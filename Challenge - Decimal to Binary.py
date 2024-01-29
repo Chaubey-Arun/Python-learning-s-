0c1a0ebbcee7e999c1d405c9078c8ee5de72124a
Arun Kumar Chaubey <chaubeyarun.arun@gmail.com>
2024-01-29 08:25:07 +0530

    Maximum Number

---
+++ Challenge - Decimal to Binary.py
@@ -0,0 +1,11 @@
+No_Number= int(input("Enter No of Number of number's "))
+max =0 
+count=0
+while No_Number>count:
+    n=int(input("Enter Number"))
+    if n>max:
+         max = n
+    else:
+         max = max 
+    count+=1
+print("maximum number", max)
