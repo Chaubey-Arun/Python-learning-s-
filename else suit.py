1058b77e076f1f577878e9f8e3b2472574b7ed50
Arun Kumar Chaubey <chaubeyarun.arun@gmail.com>
2024-01-30 07:00:07 +0530

    while loop + else =Else suit

---
+++ else suit.py
@@ -0,0 +1,10 @@
+count = 0 
+while count<10:
+    print(count)
+    count+=1
+  #  if count>5:
+   #     break  (we can use this then while - else will not work)
+else:
+    print("all 10 numbers are printed ")
+
+print("all done")
