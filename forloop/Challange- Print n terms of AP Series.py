074910de7272bcbc11c03e1834fca397fd9e69a9
Arun Kumar Chaubey <chaubeyarun.arun@gmail.com>
2024-02-01 17:35:28 +0530

    Print n terms of AP series

---
+++ forloop/Challange- Print n terms of AP Series.py
@@ -0,0 +1,6 @@
+a = int (input("Enter the 1st value  - "))
+d =int(input("Enter the common diffrence value -"))
+n = int(input ("Enter the Number you want of the series")
+    )
+for t in range(a,a+n*d,d):
+    print(t)
