70c7575436782654679f5b3fbc1be3d273680ffc
Arun Kumar Chaubey <chaubeyarun.arun@gmail.com>
2024-02-16 03:11:21 +0530

    this is method one to verfiy -my own brain

---
+++ string challange/String challange 3 (Password).py
@@ -0,0 +1,9 @@
+#check if the passowrd and confirm password are same 
+pass1= input('Enter the Password')
+pass2= input('Renter the Passowrd')
+if pass1 == pass2:
+    print('It is valid Passowrd')
+elif pass1 == pass2.isupper()or pass2.islower or pass2.capitalize or pass2.casefold :
+    print('Please check your word and re-enter')
+else:
+    print('Passwords are not same')
