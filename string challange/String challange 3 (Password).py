38674996ba9ac9197af6ef386a7141c9ade3eb62
Arun Kumar Chaubey <chaubeyarun.arun@gmail.com>
2024-02-16 03:16:27 +0530

    this is by teacher logic only by casfolding() of stirng

--- string challange/String challange 3 (Password).py
+++ string challange/String challange 3 (Password).py
@@ -3,7 +3,7 @@ pass1= input('Enter the Password')
 pass2= input('Renter the Passowrd')
 if pass1 == pass2:
     print('It is valid Passowrd')
-elif pass1 == pass2.isupper()or pass2.islower or pass2.capitalize or pass2.casefold :
+elif pass1.casefold() == pass2.casefold() :
     print('Please check your word and re-enter')
 else:
-    print('Passwords are not same')
+    print('It is not working out ')
