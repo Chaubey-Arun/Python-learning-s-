35036686721983883e5bf4d49881c478e1673b16
Arun Kumar Chaubey <chaubeyarun.arun@gmail.com>
2024-02-16 05:33:12 +0530

    convert a given string to palindrome

--- string challange/checking a string is a palindrome- string challange.py
+++ string challange/checking a string is a palindrome- string challange.py
@@ -1,8 +1,11 @@
-#checking a string is a palindrone by using string Method 
+#convert a given string to palindrome 
 str1= input('Enter to check Palindrome -')
 n=len(str1)
 str2 = str1[::-1]
-if (str1 == str2):
+str3 = str2 + str1
+print(str3)
+str4 = str3[::-1]
+if (str4 == str3):
     print('Palindrome')
 else:
     print('it is not ')
